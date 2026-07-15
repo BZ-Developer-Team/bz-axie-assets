1c374affea69940e4c18cb401ed8fbd6203aebfc
mosquito1459 <50016916+mosquito1459@users.noreply.github.com>
2026-07-15 09:32:33 +0700

    Delete web_components/types directory

--- web_components/types/inventory-stats.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Item, ItemConfig, Items } from './types';
-export type InventoryStats = {
-    totalWeight: number;
-    slots: number;
-};
-export declare const calculateInventoryStats: (items: Items<Item>, itemsConfig: Items<ItemConfig>) => InventoryStats;
-//# sourceMappingURL=inventory-stats.d.ts.map
--- web_components/types/metadata-formatter.d.ts
+++
@@ -1,7 +0,0 @@
-export type MetadataRow = {
-    key: string;
-    label: string;
-    value: string;
-};
-export declare const getMetadataRows: (metadata: Record<string, unknown> | undefined) => MetadataRow[];
-//# sourceMappingURL=metadata-formatter.d.ts.map
